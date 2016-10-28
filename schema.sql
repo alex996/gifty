75c1ba2251f3180d5619e4824e75f91178c4105b
Alex <alexnezhynsky@gmail.com>
2016-10-27 22:57:29 -0400

    Changes to schema

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE products
 	description TEXT NOT NULL,
 	category VARCHAR(50) NOT NULL,
 	price DECIMAL(8, 2) UNSIGNED NOT NULL,
-	promotion_id INT(7) NOT NULL REFERENCES promotions(id),
+	promotion_id INT(7) REFERENCES promotions(id),
 	quantity INT(5) UNSIGNED NOT NULL, -- in stock
 	status VARCHAR(20) NOT NULL CHECK status IN ('NORMAL', 'UNAVAILABLE', 'OBSOLETE'),
 	featured BOOLEAN,
@@ -16,33 +16,34 @@ CREATE TABLE promotions
 	id INT(7) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	starts_at TIMESTAMP NOT NULL,
 	ends_at TIMESTAMP NOT NULL,
-	discount DECIMAL(4, 2) UNSIGNED NOT NULL DEFAULT 0, -- % in ex: 12.50; >= 0 and <= 99.99
+	discount DECIMAL(4, 2) UNSIGNED NOT NULL DEFAULT 0 -- % in ex: 12.50; >= 0 and <= 99.99
 );
 
 CREATE TABLE reviews
 (
 	id INT(7) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	customer_id INT(7) NOT NULL REFERENCES customers(id),
-	product_id INT(7) NOT NULL REFERENCES products(id),
+	product_id INT(5) NOT NULL REFERENCES products(id),
 	comment TEXT NOT NULL,
 	rating INT(1) NOT NULL CHECK rating BETWEEN 1 AND 5,
-	created_at TIMESTAMP NOT NULL
+	created_at DATETIME NOT NULL
 );
 
 CREATE TABLE images
 (
 	id INT(7) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	product_id id INT(5) NOT NULL REFERENCES products(id),
+	product_id INT(5) NOT NULL REFERENCES products(id),
 	path VARCHAR(255) NOT NULL,
-	alt_text VARCHAR(255) NOT NULL
+	alt_text VARCHAR(255) NOT NULL,
+	featured BOOLEAN
 );
 
 CREATE TABLE users
 (
 	id INT(7) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(255) NOT NULL,
-	email VARCHAR(255) UNIQUE NOT NULL,
-	password VARCHAR(255) NOT NULL,
+	name VARCHAR(100) NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	password VARCHAR(100) NOT NULL,
 	role VARCHAR(10) NOT NULL CHECK role IN ('ADMIN', 'CUSTOMER')
 );
 
@@ -58,7 +59,7 @@ CREATE TABLE customers
 
 CREATE TABLE payment_methods
 (
-	id INT(7) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	customer_id INT(7) NOT NULL REFERENCES customers(id),
 	type VARCHAR(20) NOT NULL CHECK type IN ('VISA', 'MASTERCARD', 'INTERAC'),
 	last_digits INT(4) NOT NULL,
@@ -73,15 +74,11 @@ CREATE TABLE orders
 (
 	id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	customer_id INT(7) NOT NULL REFERENCES customers(id),
-	address VARCHAR(255) NOT NULL,
-	city VARCHAR(50) NOT NULL,
-	state VARCHAR(50) NOT NULL,
-	country VARCHAR(50) NOT NULL,
-	zip VARCHAR(10) NOT NULL,
-	created_at TIMESTAMP NOT NULL,
+	address_id INT(10) NOT NULL REFERENCES addresses(id),
+	method_id INT(10) NOT NULL REFERENCES payment_methods(id),
 	status VARCHAR(20) NOT NULL CHECK status IN ('PENDING', 'APPROVED', 'DELIVERED', 'CANCELLED', 'ERROR'),
 	total DECIMAL(8, 2) UNSIGNED NOT NULL,
-	method_id INT(7) NOT NULL REFERENCES payment_methods(id)
+	created_at TIMESTAMP NOT NULL,
 );
 
 CREATE TABLE order_details
@@ -93,10 +90,21 @@ CREATE TABLE order_details
 	quantity INT(5) UNSIGNED NOT NULL
 );
 
+CREATE TABLE addresses
+(
+	id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	customer_id INT(7) NOT NULL REFERENCES customers(id),
+	address VARCHAR(255) NOT NULL,
+	city VARCHAR(50) NOT NULL,
+	state VARCHAR(50) NOT NULL,
+	country VARCHAR(50) NOT NULL,
+	zip VARCHAR(10) NOT NULL,
+);
+
 CREATE TABLE carts
 (
 	id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	customer_id INT(7) REFERENCES users(id),
+	customer_id INT(7) REFERENCES customers(id),
 	sess_id VARCHAR(50),
 	created_at DATETIME NOT NULL
 );
