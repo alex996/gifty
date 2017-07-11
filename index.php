ab10b275bfcd00fc22cee57fae0de7ce3fbbded6
Alex <alexnezhynsky@gmail.com>
2017-07-11 04:24:52 +0100

    Symfony HttpFoundation added

--- index.php
+++ index.php
@@ -2,8 +2,21 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 
-$input = $_GET['name'] ?? 'World';
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-header('Content-Type: text/html; charset=utf-8');
+$request = Request::createFromGlobals();
+//die(nl2br($request));
+$input = $request->get('name', 'World');
 
-printf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
+$response = new Response(
+    sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8'))
+);
+
+$response->prepare($request);
+$response->send();
+
+// todos:
+// 1. write a test ;)
+// 2. review this http://symfony.com/doc/current/introduction/from_flat_php_to_symfony2.html
+// 3. continue here http://symfony.com/doc/current/create_framework/http_foundation.html
