9a2145181e4dd89ea9164acbaeab52ead74822db
Alex <alexnezhynsky@gmail.com>
2017-07-12 03:23:02 +0100

    Routing component added

--- public/index.php
+++ public/index.php
@@ -2,30 +2,33 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+
+use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 $request = Request::createFromGlobals();
-$response = new Response();
+$routes = require_once __DIR__.'/../app/routes.php';
 
-$map = [
-    '/hello' => 'hello',
-    '/bye' => 'bye'
-];
+$context = new RequestContext();
+$context->fromRequest($request);
+$matcher = new UrlMatcher($routes, $context);
 
-$path = $request->getPathInfo();
-if (isset($map[$path])) {
+try {
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
     ob_start();
-    extract($request->query->all(), EXTR_SKIP);
-    include_once sprintf(__DIR__.'/../views/%s.php', $map[$path]);
+    include_once sprintf(__DIR__.'/../views/%s.php', $_route);
+
     $response = new Response(ob_get_clean());
 }
-else {
+catch (ResourceNotFoundException $e) {
     $response = new Response('Not Found', 400);
+}
+catch(Exception $e) {
+    $response = new Response('Internal Server Error', 500);
 }
 
 $response->prepare($request);
 $response->send();
-
-
-//die(nl2br($request));
